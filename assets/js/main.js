33b56509c8acd86e38330066421f552623e6ccce
franciscoqueirozdriver <francisco.queirozdriver@gmail.com>
2025-06-21 14:34:49 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // 🔗 API
-const API_URL = 'https://SEU_LINK_DA_API/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyyK01zS9CGJdb0ONaHcUAXp3Cp4Tz4BB5Hp85FdJxjx3zK4qZm7WGG4YzH3ugT5IE/exec';
 
 // 🔥 Dark Mode
 function toggleDarkMode() {

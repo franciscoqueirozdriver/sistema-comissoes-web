1e68d4b1f91d2d4e447691035a305abb8eea3023
franciscoqueirozdriver <francisco.queirozdriver@gmail.com>
2025-06-22 13:52:03 -0300

    Update configuracoes.js

--- api/configuracoes.js
+++ api/configuracoes.js
@@ -1,8 +1,6 @@
-import { google } from 'googleapis';
-import { auth } from '../../utils/auth';
+import { sheets } from '../../utils/auth';
 
 export default async function handler(req, res) {
-  const sheets = google.sheets({ version: 'v4', auth });
   const spreadsheetId = process.env.SPREADSHEET_ID;
   const range = 'Configuracoes';
 
@@ -13,13 +11,10 @@ export default async function handler(req, res) {
         range,
       });
       res.status(200).json(response.data.values);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  }
-
-  else {
+  } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
 }
-

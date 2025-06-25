488e8e37181e1bed846c7511cb3fae5330b0e504
franciscoqueirozdriver <francisco.queirozdriver@gmail.com>
2025-06-25 01:02:11 -0300

    Update auth.js

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+export default function auth(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  next();
+}

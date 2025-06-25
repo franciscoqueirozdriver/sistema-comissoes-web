609f5cd90a7a6a5ca9d3d5ed37948fab1c2afa18
franciscoqueirozdriver <francisco.queirozdriver.com>
2025-06-24 23:52:04 -0300

    Substituição completa da estrutura do projeto

--- utils/auth.js
+++
@@ -1,8 +0,0 @@
-import { google } from 'googleapis';
-
-export const auth = new google.auth.JWT(
-  process.env.GOOGLE_CLIENT_EMAIL,     // ← Vem do Environment da Vercel
-  null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Corrige as quebras de linha da chave
-  ['https://www.googleapis.com/auth/spreadsheets']       // Permissão para acessar o Google Sheets
-);
